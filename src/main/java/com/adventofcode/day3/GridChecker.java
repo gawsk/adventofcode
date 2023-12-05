22076f8d00ed71fd6a16c29cdc8139faf69b7dd2
Morgan Larkin <16796263+gawsk@users.noreply.github.com>
2023-12-04 22:53:07 -0500

    partial work on day 3

--- src/main/java/com/adventofcode/day3/GridChecker.java
+++ src/main/java/com/adventofcode/day3/GridChecker.java
@@ -5,19 +5,21 @@ import java.io.FileReader;
 
 public class GridChecker {
     
+    public static int parseGrid(BufferedReader br) {
+        String line = br.readLine();
+
+        return 0;
+    }
 
 
     public static void main(String[] args) throws Exception {
         String filePath = "src/main/java/com/adventofcode/day3/";
 
         try(BufferedReader br = new BufferedReader(new FileReader(filePath + "input.txt"))) {
-            String line = br.readLine();
-            int total = 0;
 
-            while (line != null) {
-                total = gamePower(line);
-                line = br.readLine();
-            }
+            String line = br.readLine();
+            line.
+            int total = parseGrid(br);
             System.out.println(total);
         }
     }
